c0ae41bd3f25fb8a1c7391ff4ac43b1cd62c781f
wesley <142819760+wesleysantos0@users.noreply.github.com>
2024-03-13 23:03:59 -0300

    att-13/03/2024
    atualizando o menu do site

--- projeto-2024/js/main.js
+++ projeto-2024/js/main.js
@@ -1,1 +1,12 @@
-// .
+// adicionando btnMobile
+
+const btnMobile = document.getElementById('menuMObile')
+
+function abirMenu() {
+    const navMenu = document.getElementById('nav')
+    navMenu.classList.toggle('open')
+}
+
+btnMobile.addEventListener('click', abirMenu);
+
+
