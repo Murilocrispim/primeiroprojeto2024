a784cec1dad732e2c2c1b0870f7c651aa0585dba
wesley <142819760+wesleysantos0@users.noreply.github.com>
2024-03-16 15:05:11 -0300

    att
    atualizacaso

--- projeto-2024/js/main.js
+++ projeto-2024/js/main.js
@@ -10,3 +10,20 @@ function abirMenu() {
 btnMobile.addEventListener('click', abirMenu);
 
 
+
+// Função para trocar a logoMobile quando a largura da tela atingir 570px
+function trocarLogo() {
+    let logo = document.getElementById('logo-principal');
+    if (window.innerWidth <= 600) {
+        logo.src = 'imagens/4-removebg-preview.png';
+    } else {
+        logo.src = 'imagens/1.jpg';
+    }
+}
+
+// Chamar a função no carregamento da página e quando a tela for redimensionada
+window.onload = trocarLogo;
+window.onresize = trocarLogo;
+
+
+
